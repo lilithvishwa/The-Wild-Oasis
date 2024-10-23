ff1a79d6fbf1c254400ec896ff80b12f9c514e50
lilithvishwa <lilith04062005@gmail.com>
2024-10-23 20:27:25 +0530

    initial commit

---
+++ src/services/supabase.js
@@ -0,0 +1,17 @@
+// import { createClient } from "@supabase/supabase-js";
+
+// export const supabaseUrl = "https://dclaevazetcjjkrzczpc.supabase.co";
+// const supabaseKey =
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjbGFldmF6ZXRjamprcnpjenBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyOTIzNDQsImV4cCI6MTk5ODg2ODM0NH0.LGg0M-taoHgKtxCzr9owrb09epnPaO_Yfz6xVE54sIY";
+// const supabase = createClient(supabaseUrl, supabaseKey);
+
+// export default supabase;
+// lilith@test.com lilith@2005
+
+import { createClient } from "@supabase/supabase-js";
+export const supabaseUrl = "https://vlmwbhijptqninmdjobx.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZsbXdiaGlqcHRxbmlubWRqb2J4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU1NTA5OTAsImV4cCI6MjA0MTEyNjk5MH0.PyBaxOqEBecngk3fNLKM1t4Z4wt7Kt5Hc0mInbYKa98";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
